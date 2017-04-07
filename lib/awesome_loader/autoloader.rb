245686b940320d6d48c93840beb7ae4f43e46da2
Jordan Hollinger <jordan.hollinger@gmail.com>
2017-04-07 09:25:53 -0400

    Change finalize to finalize\!. More docs

--- lib/awesome_loader/autoloader.rb
+++ lib/awesome_loader/autoloader.rb
@@ -1,29 +1,77 @@
 require 'set'
+require 'pathname'
 
+#
+# A module that holds all the awesomeness.
+#
 module AwesomeLoader
+  #
+  # Set up an AwesomeLoader::Autoloader instance.
+  #
+  #   AwesomeLoader.autoload root_depth: 2 do
+  #     paths %w(app models ** *.rb)
+  #     paths %w(app helpers ** *.rb)
+  #     paths %w(app routes ** *.rb), root_depth: 1
+  #   end
+  #
+  # @param root_depth [String] Tells AwesomeLoader to start creating Modules for dirs *after* this level
+  # @param root [String] Path to root of the application (default Dir.cwd)
+  # @param eager_load [Boolean] Make sure all files get loaded by the time the block finishes (default false)
+  # @return [AwesomeLoader::Autoloader]
+  #
   def self.autoload(root_depth:, root: Dir.cwd, eager_load: false, &block)
     autoloader = Autoloader.new(root: root, root_depth: root_depth, eager_load: eager_load)
     if block
       autoloader.instance_eval(&block)
-      autoloader.finialize
+      autoloader.finialize!
     end
     autoloader
   end
 
+  #
+  # The autoloader. Normally it's used indirectly through AwesomeLoader.autoload, but you can use it directly if you like:
+  #
+  #   AwesomeLoader::Autoloader.new(root_depth: 2).
+  #     paths(%w(app models ** *.rb)).
+  #     paths(%w(app helpers *.rb)).
+  #     paths(%w(app routes ** *.rb), root_depth: 1).
+  #     finialize!
+  #
   class Autoloader
     RB_EXT = /\.rb$/
 
+    # @return [Pathname] the application root
     attr_reader :root
+    # @return [Integer] root depth used for all paths unless otherwise specified
     attr_reader :default_root_depth
+    # @return [Boolean] whether or not to automatically load all files once they're defined
     attr_reader :eager_load
+    # @return [Array<String>] all defined files, ready to be eager_loaded
     attr_reader :all_files
+    private :all_files
 
+    #
+    # Initialize a new AwesomeLoader::Autoloader.
+    #
+    # @param root_depth [String] Tells AwesomeLoader to start creating Modules for dirs *after* this level
+    # @param root [String] Path to root of the application (default Dir.cwd)
+    # @param eager_load [Boolean] Make sure all files get loaded by the time the block finishes (default false)
+    #
     def initialize(root_depth:, root: Dir.cwd, eager_load: false)
       @root = Pathname.new(root.to_s)
       @default_root_depth, @eager_load = root_depth, eager_load
       @all_files = []
     end
 
+    #
+    # Set a glob pattern of files to be autoloaded.
+    #
+    #   autoloader.paths %w(app models ** *.rb)
+    #
+    # @param array [Array<String>] A glob pattern as an array.
+    # @paths root_depth [Integer] Depth at which to start creating modules for dirs. Defaults to whatever the AwesomeLoader::Autoloader instance was initialized with.
+    # @return [AwesomeLoader::Autoloader] returns self, so you can chain calls
+    #
     def paths(array, root_depth: default_root_depth)
       files = Dir.glob File.join *array
       root_regex = Regexp.new "^([^/]+/){%d}" % root_depth
@@ -51,13 +99,16 @@ module AwesomeLoader
         const_name = Utils.camelize File.basename(path).sub(RB_EXT, '')
         mod = modules.fetch File.dirname path.sub(root_regex, '')
         mod.autoload const_name, full_path
-        self.all_files << full_path if eager_load
+        all_files << full_path if eager_load
       end
 
       self
     end
 
-    def finialize
+    #
+    # Perform any final operations or cleanup. If eager_load is true, this is where they're loaded.
+    #
+    def finialize!
       all_files.each { |f| require f } if eager_load
       all_files.clear
       self
--- lib/awesome_loader/utils.rb
+++ lib/awesome_loader/utils.rb
@@ -1,7 +1,17 @@
 module AwesomeLoader
+  #
+  # Misc utilities.
+  #
   module Utils
+    # Regex to match "snake name" paths
     SNAKE = /_([a-z])/
 
+    #
+    # Converts a snake_case_name to a CamelCaseName.
+    #
+    # @param name [String] the snake_case version
+    # @return [String] the CamelCase version
+    #
     def self.camelize(name)
       name.capitalize.gsub(SNAKE) { |match| match[1].capitalize }
     end
